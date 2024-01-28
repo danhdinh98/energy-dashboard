4acad0433cbc50c21dad6be57366aea7bd194822
Danh Dinh <danhdinh98@gmail.com>
2024-01-28 12:52:29 +1000

    created an energy dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import styles from "./page.module.css";
+
+export default function Dashboard() {
+    return <main className={styles.main}>
+        <h1>Electricity Usage</h1>
+        <h2>28th of January to 15 February 2024</h2>
+    </main>
+}
