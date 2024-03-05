8f2eb9354bb1c8d2a09284f7baa561a27492b5c7
Danh Dinh <danhdinh98@gmail.com>
2024-03-05 19:50:36 +1000

    improved dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,9 +8,20 @@ import BarGraph from "@/components/BarGraph";
 export default function Dashboard() {
     return <main className={styles.main}>
         <Title order={1}>Electricity Usage</Title>
-        <Title order={2} size="h4">{`${testData.startDate} to ${testData.endDate}`}</Title>
-        <SummaryTable usageData={testData} />
-        <BarGraph data={testData} title="title" description="description" />
+        <div className="dashboard-layout" style={{ backgroundColor: 'white', borderRadius: '20px', padding: '30px' }}>
+            <Title order={2} size="h4">{`${testData.startDate} to ${testData.endDate}`}</Title>
+            <SummaryTable usageData={testData} />
+        </div>
+        <div className="top" style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', backgroundColor: 'white', borderRadius: '20px', padding: '30px', marginTop: '30px' }}>
+            <h2>Monthly electricity usage in 2021</h2>
+            <BarGraph data={testData} title="title" description="description" />
+        </div>
+        <div className="middle">
+
+        </div>
+        <div className="bottom">
+
+        </div>
     </main>
 }
 
